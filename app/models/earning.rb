8f3265b96685cd9fbf91f9a0d250f49aeaf615b8
steelerx155 <steelerx155@gmail.com>
2021-02-25 07:10:16 -0500

    final commit

--- app/models/earning.rb
+++ app/models/earning.rb
@@ -1,16 +1,38 @@
-class Earning < ApplicationRecord   
+class Earning < ApplicationRecord  
 
-    def calc_taxes(amount)
-        case amount
-        when amount.between?(0, 9999)
-            return amount
-        when amount.between?(10000, 19999)
-            return amount * 0.10
-        when amount.between?(19999, 50000)
-            return amount * 0.20
-        when amount > 50000 
-            return amount * 0.30 
-        end
-        byebug
-    end
+
+    def calculate_taxes 
+        @earnings = Earning.all  
+        @tax = []  
+          @earnings.map do |e|
+             e.amount                                           
+            if e.amount < 9999
+            @tax = 0            
+            elsif e.amount.between?(10000, 19999)                    
+                 @tax = e.amount * 0.10           
+            elsif e.amount.between?(19999, 50000)                 
+                 @tax = e.amount * 0.20 
+            else e.amount > 50000  
+                 @tax = e.amount * 0.30                    
+            end                              
+          end
+          return @tax   
+        end  
+
+    # def calc_taxes(amount)
+    #     case amount
+    #     when amount.between?(0, 9999)
+    #         tax = 0
+    #         return amount
+    #     when amount.between?(10000, 19999)
+    #         tax = amount * 0.10 
+    #         return tax
+    #     when amount.between?(19999, 50000)
+    #         tax = amount * 0.20
+    #         return tax
+    #     when amount > 50000 
+    #         tax = amount * 0.30
+    #         return tax 
+    #     end
+    # end
 end
